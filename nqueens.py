16ad7932bc6737df5b0cb040c2e77575e4befdf6
TaylorElliott <taylorman@hotmail.ca>
2019-02-14 14:24:43 -0500

    Add files via upload
    Added check_conflict function

--- nqueens.py
+++ nqueens.py
@@ -1,6 +1,7 @@
 import random
 import os
 import os.path
+import operator
 
 
 def get_input_file():
@@ -27,23 +28,37 @@ def write_output_file(pendingStr):
         f.write(pendingStr)
 
 #def initialization() #start the program
+
+
+'''
+Takes in the y coordinates and returns the number of conflicts based on the x coordinate
+forward diagnol conflicts are respective piece (the first value is piece with x value of 1, etc etc
+backward diagnol is same as forward
 '''
 def check_conflict(table):
-    global table_size
-    conflict_num = 0
-    x = 1
-    conflict_in_col = []
+    conflict_y = []
+    position = 0
+    conflict_forward = []
+    conflict_backward = []
+    forwardDiagnol = []
+    backwardDiagnol = []
+    totalConflicts = []
     for i in table:
-        y = i
-        n = 1
-        for j in table:
-            if x == 
-                conflict = conflict + 1
-            if y = j:
-                conflict = conflict + 1
-                
-        x = x + 1
-'''       
+        conflict_y.append(table.count(i)-1)
+        forwardDiagnol.append((position+1)-i)
+        backwardDiagnol.append((position+1)+i)
+        position = position + 1
+    for x in forwardDiagnol:
+        conflict_forward.append(forwardDiagnol.count(x)-1)
+    for y in backwardDiagnol:
+        conflict_backward.append(backwardDiagnol.count(y)-1)
+    totalConflicts = list(map(operator.add, conflict_y, conflict_forward))
+    totalConflicts = list(map(operator.add, totalConflicts, conflict_backward))
+    return (totalConflicts)
+    
+
+    
+      
         
 
 def place_queen(table_size): # can be used for restart
@@ -70,7 +85,8 @@ def main():
     table = place_queen(10)
     print(table)
     write_output_file(outputPendingFile(table))
-
+    Conflicts = check_conflict(table)
+    print(Conflicts)
     return
 
 
